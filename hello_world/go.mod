a937a210b969865da1db7ddf1beac01684693d9d
Shreyas <shreyasks.cs18@bmsce.ac.in>
2021-07-31 16:43:36 +0530

    .

---
+++ hello_world/go.mod
@@ -0,0 +1,3 @@
+module github.com/shrey-19/GO/hello_world
+
+go 1.16
---
+++ hello_world/main.go
@@ -0,0 +1,8 @@
+package main
+import(
+ "fmt"
+ "github.com/shrey-19/Go/hello_world/sayHello"
+)
+func main(){
+ fmt.Println(sayHello.Hello())
+}
